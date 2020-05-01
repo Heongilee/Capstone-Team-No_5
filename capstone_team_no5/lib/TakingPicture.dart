ba5b9ef2e59394ff51f9a51c6c91b2355d582f1b
HeongiLee <gjsrl1@gmail.com>
2020-05-01 11:09:59 +0900

    master :: 2020-05-01 :: Exception TakingPicture.dart

--- capstone_team_no5/lib/TakingPicture.dart
+++ capstone_team_no5/lib/TakingPicture.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
 
 class TakingPicture extends StatefulWidget {
   @override
@@ -10,6 +12,7 @@ class TakingPicture extends StatefulWidget {
 
 class _TakingPictureState extends State<TakingPicture> {
   File _image;
+  List<Widget> _listViewItem = [];
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +50,7 @@ class _TakingPictureState extends State<TakingPicture> {
             Container(
               width: 300.0,
               height: 300.0,
-              // alignment: Alignment(0.0, 0.0),
-              child: (_image == null) ? _takingPictureInfo() : Image.file(_image, fit: BoxFit.cover,), // 이미지 파일 영역
+              child: (_image == null) ? Center(child: Text('No Image')) : _buildListView(),
             ),
             Padding(padding: EdgeInsets.all(16.0)),
             SizedBox(
@@ -64,15 +66,42 @@ class _TakingPictureState extends State<TakingPicture> {
     );
   }
 
-  Widget _takingPictureInfo() {
-    return Text('No Images', textAlign: TextAlign.center,);
-  }
-
   Future _getImage() async{
     File image = await ImagePicker.pickImage(source: ImageSource.camera);
 
     setState(() {
+      // 이미지 스트림이 들어오면 리스트가 repaint되는 식으로 작성!!
       _image = image;
+      
+      print(image.toString());
+      // 중간에 이미지 촬영을 안 하고 바로 넘어갔을 경우... 처리
+      if(_image != null)
+        _addlistViewItem(_image);
     });
+  }
+
+  Widget _buildListView() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemCount: _listViewItem.length,
+      scrollDirection: Axis.horizontal,
+      cacheExtent: 1500.0,
+      itemBuilder: (BuildContext context, int idx) {
+        return Container(
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.only(right: 10.0),
+              width: 300.0,
+              height: 300.0,
+              child: _listViewItem[idx],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _addlistViewItem(File image) {
+    _listViewItem.add(Image.file(image, fit: BoxFit.cover));
   }
 }
