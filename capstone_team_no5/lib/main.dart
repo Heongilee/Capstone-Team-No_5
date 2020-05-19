bc92109a732be25dcedc3693bc5cadf1d1e9fa65
HeongiLee <gjsrl1@gmail.com>
2020-05-19 10:46:37 +0900

    Merge myios branch 200519-10:46

--- capstone_team_no5/lib/main.dart
+++ capstone_team_no5/lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:recycle/ChangeMyInfo.dart';
 import 'package:page_transition/page_transition.dart';
-import 'package:recycle/MyInfo.dart';
-import 'package:recycle/NoticePage.dart';
 import 'package:recycle/RootPage.dart'; // 원래 페이지
-import 'package:recycle/SignUp.dart';
 import 'package:recycle/MainPage.dart';
 import 'package:recycle/TabPage.dart';
 import 'package:recycle/TrashListComfirmation.dart';
 import 'package:recycle/TakingPicture.dart';
-import 'package:recycle/FindAccount.dart';
 import 'package:recycle/CustomerForm.dart';
-import 'package:recycle/MyInfo.dart';
-
-import 'Example.dart';
 
 // 2020-05-07
 void main() => runApp(MyApp());
@@ -33,7 +25,7 @@ class MyApp extends StatelessWidget {
       // home: TakingPicture(),
       // home: NoticePage(),
       // home: CustomerForm(),
-      home: Example(),
+      home: RootPage(),
       routes: <String, WidgetBuilder>{
         '/MainPage': (BuildContext context) => new MainPage(),
         TakingPicture.routeName: (context) => TakingPicture(),
