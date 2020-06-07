ad4c8246c4d372db29c836a4c71d23022c4cc600
portfolioExpert <gus7518@naver.com>
2020-06-07 19:07:20 +0900

    myios :: 2020-06-07 19:07

--- capstone_team_no5/lib/MyApp_config.dart
+++ capstone_team_no5/lib/MyApp_config.dart
@@ -2,6 +2,7 @@ import 'package:path_provider/path_provider.dart'; // 앱 내부 저장소를 �
 import 'dart:io'; // 파일 읽고 쓰기 위한 라이브러리
 import 'dart:async'; // 내부 저장소 I/O 비동기 작업.
 import 'dart:convert'; // JSON 분석에 필요한 라이브러리.
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 abstract class MyAppConfigFunction {
   Future<String> get _localPath async {}
@@ -12,9 +13,11 @@ abstract class MyAppConfigFunction {
 
 class MyApp_config with MyAppConfigFunction {
   static final MyApp_config _getInstance = MyApp_config._internal();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   String _receiveID;
   bool _chkboxID;
   bool _chkboxAUTO;
+  dynamic _clientToken;
   
   String get receiveID => _receiveID;
 
@@ -28,32 +31,41 @@ class MyApp_config with MyAppConfigFunction {
 
   set chkboxAUTO(bool value) => _chkboxAUTO = value;
 
+  dynamic get client_token => _clientToken;
+
+  set client_token(dynamic client_token) => _clientToken = client_token;
+
   // MyApp_config(this._receiveID, this._chkboxID, this._chkboxAUTO);
   factory MyApp_config() => _getInstance;
 
   MyApp_config._internal(
-      {String receiveID = "", bool chkboxID = false, bool chkboxAUTO = false}) {
+      {String receiveID = "", bool chkboxID = false, bool chkboxAUTO = false, dynamic client_token = ""}) {
     this._receiveID = receiveID;
     this._chkboxID = chkboxID;
     this._chkboxAUTO = chkboxAUTO;
+    _getMyAccessToken().then((dynamic token) {
+      this._clientToken = token;
+    });
 
     print('MyApp_config Singleton Class was created...');
   }
 
   @override
   String toString() {
-    return "{ ${this._receiveID}, ${this._chkboxID}, ${this._chkboxAUTO} }";
+    return "{ ${this._receiveID}, ${this._chkboxID}, ${this._chkboxAUTO}, ${this._clientToken} }";
   }
 
   MyApp_config.fromJson(Map<String, dynamic> json)
       : _receiveID = json['receiveID'],
         _chkboxID = json['chkboxID'],
-        _chkboxAUTO = json['chkboxAUTO'];
+        _chkboxAUTO = json['chkboxAUTO'],
+        _clientToken = json['clientToken'];
 
   Map<String, dynamic> toJson() => {
         "receiveID": this._receiveID,
         "chkboxID": this._chkboxID,
-        "chkboxAUTO": this._chkboxAUTO
+        "chkboxAUTO": this._chkboxAUTO,
+        "clientToken": this._clientToken,
       };
 
   @override
@@ -93,6 +105,12 @@ class MyApp_config with MyAppConfigFunction {
 
       return MyApp_config._getInstance;
     }
+  }
+
+  Future<dynamic> _getMyAccessToken() async{
+    dynamic myToken = await _firebaseMessaging.getToken();
+
+    return myToken;
   }
 }
 MyApp_config myapp_config_instance = MyApp_config();
